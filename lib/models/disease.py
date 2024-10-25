b6d7287b0786ab82377718e7d1df9fc1de882170
Amene <layla_1380@yahoo.com>
2024-10-25 10:23:20 -0500

    Add Disease class with attributes and validation

--- lib/models/disease.py
+++ lib/models/disease.py
@@ -2,3 +2,35 @@
 
 class Disease:
     all = {}
+
+    def __init__(self, name, symptoms=None, id=None):
+        self.id = id
+        self.name = name
+        self.symptoms = symptoms or []
+
+    def __repr__(self):
+        return f'<Disease {self.id}: {self.name}>'
+
+  
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError('Name must be a non-empty string.')
+
+    
+    @property
+    def symptoms(self):
+        return self._symptoms
+
+    @symptoms.setter
+    def symptoms(self, symptoms):
+        if isinstance(symptoms, list) and len(symptoms) > 0:
+            self._symptoms = symptoms
+        else:
+            raise ValueError('Symptoms must be a non-empty list.')
