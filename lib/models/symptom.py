0e6cafcc62eea39f981588b4ec86616413d82ac3
Amene <layla_1380@yahoo.com>
2024-10-25 10:35:49 -0500

    Add create_table, drop_table, and save methods to Symptom class

--- lib/models/symptom.py
+++ lib/models/symptom.py
@@ -25,3 +25,49 @@ class Symptom:
         else:
             raise ValueError('Description must be string between 3 and 50 characters inclusive.')
     
+    @classmethod
+    def create_table(cls):
+        from lib.cli import CURSOR, CONN 
+        '''Create the symptoms table in the database.'''
+        sql = '''
+            CREATE TABLE IF NOT EXISTS symptoms(
+            id INTEGER PRIMARY KEY, description TEXT,
+            patient_id INTEGER,
+            disease_id INTEGER,
+            FOREIGN KEY (patient_id) REFERENCES patients(id),
+            FOREIGN KEY (disease_id) REFERENCES diseases(id)
+            )
+        '''
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+
+
+
+
+
+
+    @classmethod
+    def drop_table(cls):
+        from lib.cli import CURSOR, CONN
+        '''Drop the symptoms table from the database.'''
+        sql = '''
+            DROP TABLE IF EXISTS symptoms
+        '''
+        CURSOR.execute(sql)
+        CONN.commit()
+
+
+
+    def save(self):
+        from lib.cli import CURSOR, CONN 
+        '''Insert the Symptom instance into the database and save the ID.'''
+        sql = '''
+            INSERT INTO symptoms(description, patient_id, disease_id) 
+            VALUES(?,?,?)  
+        '''
+        CURSOR.execute(sql, (self.description, self.patient_id, self.disease_id))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
