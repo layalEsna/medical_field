8f647e56168b6d389eed769766d5b4943f1628ad
Amene <layla_1380@yahoo.com>
2024-10-24 22:57:18 -0500

    Add create_table class method to Patient class

--- lib/models/patient.py
+++ lib/models/patient.py
@@ -48,6 +48,20 @@ class Patient:
             self._age = age
         else:
             raise ValueError('Age must be an integer between 18 and 100 inclusive.')
+        
+    @classmethod
+    def create_table(cls):
+        from lib.cli import CURSOR, CONN 
+        sql = '''
+          CREATE TABLE IF NOT EXISTS patients(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            last_name TEXT,
+            age INTEGER
+            )
+        '''
+        CURSOR.execute(sql)
+        CONN.commit()
 
 
 
