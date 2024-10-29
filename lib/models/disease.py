e1c95b4cf7fe3fbdcb52ec620cc43f56dc00050f
Amene <layla_1380@yahoo.com>
2024-10-29 14:58:20 -0500

    Modify find_by_id, find_by_name functions in Disease class to include disease symptoms

--- lib/models/disease.py
+++ lib/models/disease.py
@@ -9,7 +9,8 @@ class Disease:
         self.symptoms = symptoms or []
 
     def __repr__(self):
-        return f'<Disease {self.id}: {self.name}>'
+        symptom_list = ', '.join(self.symptoms) if self.symptoms else None
+        return f'<Disease {self.id}: {self.name}, Symptoms: {symptom_list}>'
 
   
     @property
@@ -136,32 +137,6 @@ class Disease:
             diseases.append(disease)
         return diseases
     
-
-    @classmethod
-    def find_by_id(cls, id):
-        '''Find and return a Disease instance by ID from the diseases table.'''
-        from lib.cli import CURSOR  
-        sql = '''
-            SELECT *
-            FROM diseases
-            WHERE id = ?
-        '''
-        row = CURSOR.execute(sql, (id,)).fetchone()
-        return cls.instance_from_db(row) if row else None
-    
-    @classmethod
-    def find_by_name(cls, name):
-        '''Find and return a Disease instance by name from the diseases table.'''
-        from lib.cli import CURSOR  
-        sql = '''
-            SELECT *
-            FROM diseases
-            WHERE name = ?
-        '''
-        row = CURSOR.execute(sql, (name,)).fetchone()
-        return cls.instance_from_db(row) if row else None
-    
-
     def get_disease_symptoms(self):
         from lib.models.symptom import SymptomEntry  # Moved import here
         from lib.cli import CURSOR  # Moved import here
@@ -174,8 +149,58 @@ class Disease:
         rows = CURSOR.execute(sql, (self.id,)).fetchall()
         for row in rows:
             symptoms = SymptomEntry.instance_from_db(row)
-            disease_symptoms.append(symptoms)
+            if symptoms:
+               disease_symptoms.append(symptoms.description)
+            
         return disease_symptoms if disease_symptoms else []
+
+
+    @classmethod
+    def find_by_id(cls, id):
+        '''Find and return a Disease instance by ID from the diseases table, including associated symptoms.'''
+        from lib.cli import CURSOR  
+        sql = '''
+            SELECT *
+            FROM diseases
+            WHERE id = ?
+        '''
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        disease = cls.instance_from_db(row) if row else None
+        if disease:
+            disease.symptoms = disease.get_disease_symptoms()
+        return disease
+    
+    @classmethod
+    def find_by_name(cls, name):
+        '''Find and return a Disease instance by name from the diseases table.'''
+        from lib.cli import CURSOR  
+        sql = '''
+            SELECT *
+            FROM diseases
+            WHERE name = ?
+        '''
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        disease = cls.instance_from_db(row) if row else None
+        if disease:
+           disease.symptoms = disease.get_disease_symptoms()
+        return disease
+        
+    
+
+    # def get_disease_symptoms(self):
+    #     from lib.models.symptom import SymptomEntry  # Moved import here
+    #     from lib.cli import CURSOR  # Moved import here
+    #     disease_symptoms = []
+    #     sql = '''
+    #         SELECT *
+    #         FROM symptoms 
+    #         WHERE disease_id = ?
+    #     '''
+    #     rows = CURSOR.execute(sql, (self.id,)).fetchall()
+    #     for row in rows:
+    #         symptoms = SymptomEntry.instance_from_db(row)
+    #         disease_symptoms.append(symptoms)
+    #     return disease_symptoms if disease_symptoms else []
     
     # @classmethod
     # def disease_symptoms(cls, disease_id):
